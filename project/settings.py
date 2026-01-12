aad48a88a2c5d9f1725f0f7c0647eae6d9dfb6b5
Ailton Ferreira <ailtonfsilva05@gmail.com>
2026-01-12 07:35:21 -0300

    inserindo css e incluindo dados

--- project/settings.py
+++ project/settings.py
@@ -133,3 +133,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
