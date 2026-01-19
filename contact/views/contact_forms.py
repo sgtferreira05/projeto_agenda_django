0f96cab2a80de5903366cf824d630fffdb7b08b8
Ailton Ferreira <ailtonfsilva05@gmail.com>
2026-01-19 09:12:08 -0300

    forms

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,54 @@
+from typing import Any
+
+from django import forms
+from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from contact.models import Contact
 # Create your views here.
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            None,
+            ValidationError(
+                'Custom clean method error.',
+                code='invalid',
+            )
+        )
+        self.add_error(
+            None,
+            ValidationError(
+                'Custom error.',
+                code='invalid',
+            )
+        )
+
+        return super().clean()
 
 def create(request):
-    context = {
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST),
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+
+
+    context = {
+        'form': ContactForm(),
     }
 
     return render(
